1de6a5bb04608c65612ad084b18f74c95c5a95c6
Daniel Penelva <d4n.andrade@gmail.com>
2024-01-31 02:55:53 -0300

    criando o banner (https://devops.datenkollektiv.de/banner.txt/index.html) + criando as entidades Student e Course e a conexão com o BD MySQL (application.properties)

---
+++ src/main/java/com/api/springbootgraphql/entities/Course.java
@@ -0,0 +1,31 @@
+package com.api.springbootgraphql.entities;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "course")
+@Data
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private String teacher;
+    
+    // Um curso para muitos estudantes 
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, targetEntity = Student.class) 
+    private List<Student> studentList;
+}
