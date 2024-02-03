21317146092c127ef7490db58dc8ae32e58ab64d
Daniel Penelva <d4n.andrade@gmail.com>
2024-02-02 21:44:05 -0300

    Criando o import.sql para criar os registros no BD + add a propriedade graphiql para habilitar a interface graphiQL + criando schema graphqls - fazendo consulta de leitura (type query)

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO course (name, category, teacher) VALUES ('Matematica', 'Ciencias exatas', "Juan Diego Ossa");
+INSERT INTO course (name, category, teacher) VALUES ('Biologia', 'Ciencias', "Leandro Jarammillo");
+INSERT INTO course (name, category, teacher) VALUES ('Programacao', 'Tecnologia', "Neiro Figueroa");
+
+INSERT INTO student (name, last_name, age, course_id) VALUES ('John', 'Serna', '26', 1);
+INSERT INTO student (name, last_name, age, course_id) VALUES ('Andrea', 'Calle', '21', 2);
+INSERT INTO student (name, last_name, age, course_id) VALUES ('Anyi', 'Hoyos', '22', 1);
+INSERT INTO student (name, last_name, age, course_id) VALUES ('Santiago', 'Perez', '24', 3);
