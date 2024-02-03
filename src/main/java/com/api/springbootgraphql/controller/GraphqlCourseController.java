df553ebc662adb262c8004a31056ce30955a5462
Daniel Penelva <d4n.andrade@gmail.com>
2024-02-03 20:21:01 -0300

    Criando as mutation criar e deletar do course do schema graphiql +  GraphqlCourseController (parte 3 - fazendo mutation createCourse, deleteById)

--- src/main/java/com/api/springbootgraphql/controller/GraphqlCourseController.java
+++ src/main/java/com/api/springbootgraphql/controller/GraphqlCourseController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import com.api.springbootgraphql.entities.Course;
+import com.api.springbootgraphql.graphiql.InputCourse;
 import com.api.springbootgraphql.service.ICourseService;
 
 @Controller
@@ -26,5 +28,25 @@ public class GraphqlCourseController {
     @QueryMapping(name = "findAllCourses")
     public List<Course> findall() {
         return iCourseService.findAll();
+    }
+
+    @MutationMapping
+    public Course createCourse(@Argument InputCourse inputCourse){
+        
+        Course course = new Course();
+        course.setName(inputCourse.getName());
+        course.setCategory(inputCourse.getCategory());
+        course.setTeacher(inputCourse.getTeacher());
+
+        iCourseService.createCourse(course);
+        return course;
+
+    }
+
+    @MutationMapping(name = "deleteCourseById")
+    public String deleteById(@Argument(name = "courseId") String id){
+        
+        iCourseService.deleteById(Long.parseLong(id));
+        return "course with id " + id + " successfully deleted!";
     }
 }
